f10f4e6f6d474035eea69a5313631620129f3d3c
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:53 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostRangeConan(ConanFile):
     name = "Boost.Range"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-range"
     source_url = "https://github.com/boostorg/range"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -30,7 +31,7 @@ class BoostRangeConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):

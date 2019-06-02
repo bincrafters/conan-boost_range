879aef216cd529ffaff8a9f5e95f1603b6753228
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:07 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostRangeConan(base.BoostBaseConan):
     name = "boost_range"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_range"
-    lib_short_names = ["range"]
-    header_only_libs = ["range"]
-    cycle_group = "boost_cycle_group_a"
-    b2_requires = ["boost_cycle_group_a"]
+    version = "1.70.0"
